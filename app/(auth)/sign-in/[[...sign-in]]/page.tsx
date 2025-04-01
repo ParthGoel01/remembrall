1360b4c0378ed5e20f144f9136fa48b5fd106108
Parth Goel <146020919+ParthGoel01@users.noreply.github.com>
2025-04-01 17:43:02 +0530

    Improved Auth UI

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,14 @@ export default function Page() {
   return (
         <section className='flex justify-center items-center lg:min-h-[40vh] '>
             <div className='py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 lg:pt-12'>
-                <SignIn />
+                <SignIn
+                    appearance={{
+                        variables: {
+                            colorPrimary: "#2a3ef5",
+                            colorBackground: "#FFFFFF",
+                        }
+                    }}
+                />
             </div>
         </section>
     )
